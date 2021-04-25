233158b75cb18876ddeb80f5ac73123c5b7b1715
André Felipe Dias <andre.dias@pronus.io>
2021-04-25 13:53:41 -0300

    remove unused models and fields

--- app/models.py
+++ app/models.py
@@ -1,9 +1,7 @@
-from datetime import datetime, timezone
 from typing import Optional
 
 import orjson
 from pydantic import BaseModel as _BaseModel
-from pydantic import validator
 
 # ref: https://pydantic-docs.helpmanual.io/usage/exporting_models/#custom-json-deserialisation
 
@@ -23,7 +21,6 @@ Sourcefiles = dict[str, str]
 
 
 class Command(BaseModel):
-    type: Optional[str] = None
     command: str
     timeout: float = 0.1
     stdin: Optional[str] = None
@@ -38,28 +35,3 @@ class Response(BaseModel):
     stdout: str = ''
     stderr: str = ''
     exit_code: int = 0
-
-
-class ProjectDescriptionCore(ProjectCore):
-    title: str = ''
-    description: str = ''
-
-
-class ProjectResponses(BaseModel):
-    id: str
-    responses: list[Response] = []
-
-
-class Project(ProjectDescriptionCore, ProjectResponses):
-    timestamp: Optional[datetime] = None
-
-    @validator('timestamp', pre=True, always=True)
-    def set_ts_now(cls, v):
-        """
-        See: https://pydantic-docs.helpmanual.io/usage/validators/#validate-always
-
-        This validation might become unnecessary in Pydantic 2.0
-        and be replaced by something like 'timestamp: datetime = datetime.now
-        see: https://github.com/samuelcolvin/pydantic/pull/12108
-        """
-        return v or datetime.now(timezone.utc)
