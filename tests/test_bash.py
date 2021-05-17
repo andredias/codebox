6a1eb8470686c0bd25d24d57db9130f17d8c2269
André Felipe Dias <andre.dias@pronus.io>
2021-05-17 19:27:43 -0300

    close #19 | tests for bash Codebox support

---
+++ tests/test_bash.py
@@ -0,0 +1,35 @@
+from httpx import AsyncClient
+from pydantic import parse_obj_as
+from pytest import fixture
+
+from app.models import Command, ProjectCore, Response
+
+
+@fixture
+def run_bash(client: AsyncClient):
+    async def _run_bash(code: str) -> Response:
+        sources = {'test.sh': code}
+        commands = [
+            Command(command='/bin/bash test.sh', timeout=1, pids_max=2),
+        ]
+        resp = await client.post(
+            '/execute', json=ProjectCore(sources=sources, commands=commands).dict()
+        )
+        return Response(**(resp.json()[0]))
+
+    return _run_bash
+
+
+async def test_hello_world(run_bash):
+    code = 'echo -n "Ola mundo!"'
+    resp = await run_bash(code)
+    assert resp == Response(stdout='Ola mundo!', stderr='', exit_code=0)
+
+
+async def test_isort(run_bash):
+    code = '''python --version
+rustc --version
+'''
+    resp = await run_bash(code)
+    assert len(resp.stdout.splitlines()) == 2
+    assert resp.exit_code == 0
