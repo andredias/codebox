2daceeb0b3fa7be118ac752f49203fd97595d6e2
André Felipe Dias <andre.dias@pronus.io>
2022-06-07 12:23:48 -0300

    new tests for Response

---
+++ tests/test_models.py
@@ -0,0 +1,9 @@
+from app.models import Response
+
+
+def test_response():
+    resp1 = Response(stdout='Hello World!', stderr='', exit_code=0)
+    resp2 = Response(stdout='Hello World!', stderr='', exit_code=0, elapsed_time=0.1)
+    assert resp1 == resp2
+    assert resp1 != Response(stdout='Hello World!', stderr='', exit_code=1)
+    assert '100ms' in str(resp2)
